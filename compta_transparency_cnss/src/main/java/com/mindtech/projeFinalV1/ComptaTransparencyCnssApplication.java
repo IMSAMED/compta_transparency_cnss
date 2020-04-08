5b23ab3fda46430e9a0ecddb69a180bb942a2d2f
IMSAMED <IMSAMED@users.noreply.github.com>
2020-04-08 05:56:40 +0000

    This is where it all begins

---
+++ compta_transparency_cnss/src/main/java/com/mindtech/projeFinalV1/ComptaTransparencyCnssApplication.java
@@ -0,0 +1,13 @@
+package com.mindtech.projeFinalV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComptaTransparencyCnssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComptaTransparencyCnssApplication.class, args);
+	}
+
+}
